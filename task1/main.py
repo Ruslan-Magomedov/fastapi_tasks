53361da2e0ec2c5a8864952dc2c34ce8470dce1f
Magomedow Ruslan <main@mail.ru>
2025-05-14 21:43:56 +0300

    task1 is done

---
+++ task1/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import asyncio
+import uvicorn
+
+import random
+
+
+app = FastAPI()
+
+
+@app.get("/", summary="home page")
+async def home() -> dict:
+    """
+    Асинхронная функция генерирует случайное число от 1 до 5
+    затем засыпает n секунд и возвращает словарь Hello, World!
+    """
+    delay = random.randint(1, 5)
+    await asyncio.sleep(delay)
+    return {"message": "Hello, World!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
